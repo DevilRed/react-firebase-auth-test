62ceed5e2183ecb5677459feef4e46171a970bdc
Christian <chrisdaq@gmail.com>
2023-11-16 14:56:47 -0400

    set validations

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,64 +4,80 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [notice, setNotice] = useState("");
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [notice, setNotice] = useState("");
 
-    const loginWithUsernameAndPassword = async (e) => {
-        e.preventDefault();
+  const loginWithUsernameAndPassword = async (e) => {
+    e.preventDefault();
 
-        try {
-            await signInWithEmailAndPassword(auth, email, password);
-            navigate("./profile");
-        } catch {
-            setNotice("You entered a wrong username or password.");
-        }
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("./profile");
+    } catch (error) {
+      console.log(error);
+      setNotice("You entered a wrong username or password.");
     }
+  };
 
-    return(
-        <div className="container">
-					<h3 className="fs-4 text-center">Login</h3>
-            <div className="row justify-content-center">
-                <form className="col-md-4 mt-3 pt-3 pb-3">
-                    { "" !== notice &&
-                        <div className = "alert alert-warning" role="alert">
-                            { notice }
-                        </div>
-                    }
-                    <div className="form-floating mb-3">
-                        <input
-													type="email"
-													className="form-control"
-													id="exampleInputEmail1"
-													aria-describedby="emailHelp"
-													placeholder="name@example.com"
-													value={ email } onChange = { (e) => setEmail(e.target.value) }>
-												</input>
-                        <label htmlFor = "exampleInputEmail1" className = "form-label">Email address</label>
-                    </div>
-                    <div className = "form-floating mb-3">
-                        <input
-													type="password"
-													className="form-control"
-													id="exampleInputPassword1"
-													placeholder="Password"
-													value={ password }
-													onChange={ (e) => setPassword(e.target.value) }>
-												</input>
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    </div>
-                    <div className="d-grid">
-                        <button type="submit" className="btn btn-primary pt-3 pb-3" onClick={(e) => loginWithUsernameAndPassword(e)}>Submit</button>
-                    </div>
-                    <div className="mt-3 text-center">
-                        <span>Need to sign up for an account? <Link to = "./signup">Click here.</Link></span>
-                    </div>
-                </form>
+  return (
+    <div className="container">
+      <h3 className="fs-4 text-center">Login</h3>
+      <div className="row justify-content-center">
+        <form
+          className="col-md-4 mt-3 pt-3 pb-3"
+          onSubmit={loginWithUsernameAndPassword}
+        >
+          {"" !== notice && (
+            <div className="alert alert-warning" role="alert">
+              {notice}
             </div>
-        </div>
-    )
-}
+          )}
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              required
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              required
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary pt-3 pb-3">
+              Submit
+            </button>
+          </div>
+          <div className="mt-3 text-center">
+            <span>
+              Need to sign up for an account?{" "}
+              <Link to="./signup">Click here.</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
